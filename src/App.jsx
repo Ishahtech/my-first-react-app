5c7a131326eb4939fd379d0d7b4ca8c199945352
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-02-19 23:52:19 +0100

    feat:installed react with vite and created neccessary components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+
+
+function App() {
+  return (
+    <>
+     <Header />  
+     <Login /> 
+     <Signup />
+    </> 
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <>
+     <h1>My First React App</h1> 
+    </>
+  )
+}
+
+export default Header
